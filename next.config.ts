aec038b15cb8af88cbe612e066a4eeb8f59e9369
Raffy626 <reshainapasha@gmail.com>
2026-02-21 10:37:25 +0700

    feat: Enable Turbopack experimental feature in Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-import type { NextConfig } from "next";
 import withPWAInit from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const nextConfig: any = {
   experimental: {
-    // turbopack: {}, // If needed to bypass the webpack detection
+    turbopack: {},
   }
 };
 
